ec194d7a8fbdfadeed6fe29f99d0922cd996faff
EC2 Default User <ec2-user@ip-172-31-1-104.ec2.internal>
2021-12-15 11:00:15 +0000

    finish tske2

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,4 @@
-class List < ApplicationRecord
+class Book < ApplicationRecord
 
   validates :title, presence: {message: "can't be blank"}
   validates :body, presence: {message: "can't be blank"}
--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 
 ActiveRecord::Schema.define(version: 2021_12_13_133051) do
 
-  create_table "lists", force: :cascade do |t|
+  create_table "books", force: :cascade do |t|
     t.string "title"
     t.string "body"
     t.datetime "created_at", null: false
